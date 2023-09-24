4d01fb51fbd55353129415d5c886670728ca2fa4
lenzlaww <yuwenqian.chen@stonybrook.edu>
2023-09-24 04:10:07 -0400

    Initial Commit

---
+++ SampleHealthkit/SampleHealthkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleHealthkitApp.swift
+//  SampleHealthkit
+//
+//  Created by yuwenqian chen on 9/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SampleHealthkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
